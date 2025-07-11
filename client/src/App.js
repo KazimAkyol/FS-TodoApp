1d6882760a2d0e2d67d02ab90087456ed97c2634
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-07-11 12:12:08 +0200

    updated index.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // javascriptsel hareketli kodlar için bootstrap in script teşekkürünü index.html e yaptık
 import Home from "./pages/Home";
 
-
-
 function App() {
-  return (
-    <div >
-    <Home/>
-    </div>
-  );
+    return (
+        <div >
+            <Home />
+        </div>
+    );
 }
 
 export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,64 +2,65 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import BilgiList from "../components/BilgiList";
 import AddBilgi from "../components/AddBilgi";
+
 const Home = () => {
-  const [tutorials, setTutorials] = useState([]);
+    const [tutorials, setTutorials] = useState([]);
 
-  //   const url = "https://tutorial-api.fullstack.clarusway.com/tutorials/"; //* bu link canli'da,ulasilabilir.
-  // const url = "http://127.0.0.1:8000/todo/"; //* bu link canli'da degil, herkes tarafindan ulasilabilir olmasi icin deploy edilmesi lazim. Benim DB'im, server klasörünün icinde db.sqlite3'de.
-  //* Sadece bu url eklenerek Backend ile Frontend arasinda baglanti kuruldu.
+    //   const url = "https://tutorial-api.fullstack.clarusway.com/tutorials/"; //* bu link canli'da,ulasilabilir.
+    // const url = "http://127.0.0.1:8000/todo/"; //* bu link canli'da degil, herkes tarafindan ulasilabilir olmasi icin deploy edilmesi lazim. Benim DB'im, server klasörünün icinde db.sqlite3'de.
+    //* Sadece bu url eklenerek Backend ile Frontend arasinda baglanti kuruldu.
 
-  const url = "https://fs-todoapp-server10.onrender.com/todo"; //* deploy edildikten sonra bize render sitesinden verilen link buraya yapistirilir.
+    const url = "https://fs-todoapp-server10.onrender.com/todo"; //* deploy edildikten sonra bize render sitesinden verilen link buraya yapistirilir.
 
-  //! GET (READ)
-  const getBilgiler = async () => {
-    const res = await axios.get(url);
-    console.log(res.data);
+    //! GET (READ)
+    const getBilgiler = async () => {
+        const res = await axios.get(url);
+        console.log(res.data);
 
-    setTutorials(res.data.result);
-  };
+        setTutorials(res.data.result);
+    };
 
-  useEffect(() => {
-    getBilgiler();
-  }, []);
+    useEffect(() => {
+        getBilgiler();
+    }, []);
 
-  //! POST (create: database'e veri gönderme)
+    //! POST (create: database'e veri gönderme)
 
-  const postBilgi = async (yeniVeri) => {
-    await axios.post(url, yeniVeri);
+    const postBilgi = async (yeniVeri) => {
+        await axios.post(url, yeniVeri);
 
-    getBilgiler();
-  };
+        getBilgiler();
+    };
 
-  //! DELETE (database'den silme)
+    //! DELETE (database'den silme)
 
-  const deleteBilgi = async (id) => {
-    // console.log(id);
+    const deleteBilgi = async (id) => {
+        // console.log(id);
 
-    await axios.delete(`${url}${id}/`);
+        await axios.delete(`${url}${id}/`);
 
-    getBilgiler();
-  };
+        getBilgiler();
+    };
 
-  //! PUT (update işlemi)
+    //! PUT (update işlemi)
 
-  const putBilgi = async (yeniVeri) => {
-    await axios.put(`${url}${yeniVeri.id}/`, yeniVeri);
+    const putBilgi = async (yeniVeri) => {
+        await axios.put(`${url}${yeniVeri.id}/`, yeniVeri);
 
-    getBilgiler();
-  };
+        getBilgiler();
+    };
 
-  return (
-    <>
-      <AddBilgi postBilgi={postBilgi} />
+    return (
+        <>
+            <AddBilgi postBilgi={postBilgi} />
 
-      <BilgiList
-        tutorials={tutorials}
-        deleteBilgi={deleteBilgi}
-        putBilgi={putBilgi}
-      />
-    </>
-  );
+            <BilgiList
+                tutorials={tutorials}
+                deleteBilgi={deleteBilgi}
+                putBilgi={putBilgi}
+            />
+        </>
+    );
 };
 
 export default Home;
